829b38a4f82fc637afcc7cad3129deb7ff1da114
Vladimir <vova.krypenichka@yandex.ru>
2022-10-21 19:33:35 +0300

    Добавил массив в task2

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -3,33 +3,16 @@ package src.ru.croc.task2;
 public class Task2 {
     public static void main(String[] args) {
         double b = Double.parseDouble(args[0]);
-        String str = "B"; //единицы измерения
+        String[] units = new String[] {"B", "KB","MB", "GB", "TB", "PB"}; //единицы измерения
+
         int i = 0;
         while (b >= 1024.0) {
             b /= 1024.0;
             i++;
-            if(i == 5) {
+            if(i == units.length - 1) {
                 break;
             }
         }
-        switch (i) {
-            case 1:
-                str = "KB";
-                break;
-            case 2:
-                str = "MB";
-                break;
-            case 3:
-                str = "GB";
-                break;
-            case 4:
-                str = "TB";
-                break;
-            case 5:
-                str = "PB";
-                break;
-        }
-
-        System.out.printf("%.1f %s\n", b, str);
+        System.out.printf("%.1f %s\n", b, units[i]);
     }
 }
