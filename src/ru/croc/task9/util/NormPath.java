1e3fc5bfce3a7026976759273779c5b85a65222b
Vladimir <vova.krypenichka@yandex.ru>
2022-11-09 13:40:59 +0300

    Исправил task9

--- src/ru/croc/task9/util/NormPath.java
+++ src/ru/croc/task9/util/NormPath.java
@@ -2,7 +2,6 @@ package src.ru.croc.task9.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class NormPath {
     public static String normPath (String path) {
@@ -11,23 +10,34 @@ public class NormPath {
         String[] dir = path.split("/");
 
         for(String str : dir) {
-            if(str.charAt(0) == '.') {
-                if(str.length() == 2) {
-                    if(listPath.isEmpty()) {
-                        listPath.add("..");
-                    } else {
-                        listPath.remove(listPath.size() - 1);
-                    }
-                }
-            } else {
+            if(str.charAt(0) == '.') { //проверка на . и ..
+                toThePreviousDirectory(str, listPath);
+            } else { // если это директория
                 listPath.add(str);
             }
         }
 
-        for (String str : listPath) {
-            normPath.append(str).append("/");
+        for(int i = 0; i < listPath.size() - 1; i++) { // преобразовать в вид dir/dir1/dir2/
+            normPath.append(listPath.get(i))
+                    .append('/');
         }
 
-        return normPath.deleteCharAt(normPath.length() - 1).toString();
+        return listPath.isEmpty() ?
+               "" :
+               normPath.append(listPath.get(listPath.size() - 1)).toString(); //добавить последний элемент без /
+    }
+
+    private static void toThePreviousDirectory (String str, List<String> listPath) {
+        if(str.length() == 2) { // проверка на ..
+            if(listPath.size() == 1) { //если последний элемент .. или директория
+                if(!listPath.get(0).equals("..")) { //если директория, то удалить
+                    listPath.remove(0);
+                }
+            } else if (listPath.isEmpty()) { //если список пуст, то добавить ..
+                listPath.add("..");
+            } else {
+                listPath.remove(listPath.size() - 1); //вернуться к предыдущей директории
+            }
+        }
     }
 }
