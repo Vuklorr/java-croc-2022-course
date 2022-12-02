7902030b8b4a8e85f6021ff28864bfdbb3f55dbc
Vladimir <vova.krypenichka@yandex.ru>
2022-12-03 02:03:46 +0300

    Исправил task15

--- src/ru/croc/task15/division/FormingResponse.java
+++ src/ru/croc/task15/division/FormingResponse.java
@@ -1,55 +1,37 @@
 package ru.croc.task15.division;
 
-import ru.croc.task15.division.util.ParseFile;
-
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FormingResponse {
 
     /**
      * Находит время формирования запроса.
      *
-     * @return - время запроса в часах
+     * @return - максимальная общая продолжительность формирования ответа
      */
     public int timeFormingResponse() throws FileNotFoundException {
-        List<Division> divisionsList = new ArrayList<>();
-        ParseFile.parseFile("src/ru/croc/task15/division/file/application", divisionsList);
-        int max = getDivisionOverallDuration(divisionsList.get(0));
+        File file = new File("src/ru/croc/task15/division/file/application");
+        Scanner in = new Scanner(file);
+        Map<String, Integer> divisions = new HashMap<>();
+        int maxOverallDuration = -1;
 
-        for(int i = 1; i < divisionsList.size(); i++) {
-            int overallDuration = getDivisionOverallDuration(divisionsList.get(i));
-            if(overallDuration > max) {
-                max = overallDuration;
+        while (in.hasNextLine()) {
+            String[] data = in.nextLine().split(",");
+            int overallDuration = Integer.parseInt(data[2]);
+
+            if (!data[1].equals("-")) { // если не родитель
+                overallDuration += divisions.get(data[1]); // прибавить продолжительность обработки родителя
+            }
+            divisions.put(data[0], overallDuration);
+
+            if (overallDuration > maxOverallDuration) {
+                maxOverallDuration = overallDuration;
             }
         }
-        return max;
-    }
+        in.close();
 
-    /**
-     * Возвращает общее время, за которое подразделение обрабатывает заявку.
-     *
-     * @param division - подразделение
-     * @return - время обработки заявки в часах
-     */
-
-    private int getDivisionOverallDuration(Division division) {
-        return getDivisionOverallDuration(division, 0);
-    }
-
-    /**
-     * Рекурсивный метод нахождения полной обработки заявки.
-     *
-     * @param division - подразделение
-     * @param count - время обработки
-     * @return время обработки заявки
-     */
-    private int getDivisionOverallDuration(Division division, int count) {
-        count += division.getProcessingTime();
-        if(division.getParent() == null) {
-            return count;
-        }
-        return getDivisionOverallDuration(division.getParent(), count);
+        return maxOverallDuration;
     }
 }
